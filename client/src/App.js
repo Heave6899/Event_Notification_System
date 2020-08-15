086cb73de582aabcf1bb74a6d77a41ccaf256196
Tanuj Gupta <vanshfpatel@outlook.com>
2020-08-16 00:05:02 +0530

    Push Notification via Firebase-admin

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, {  Component } from 'react'
+import firebase from './firebase'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+  componentDidMount(){
+    const messaging = firebase.messaging()
+    messaging.requestPermission().then(()=>{
+      return messaging.getToken()
+    }).then(token=>{
+      console.log('Token :',token)
+    }).catch(()=>{
+      console.log('error')
+    })
+  }
+  render() {
+    return ( <div> </div>)
+    }
+  }
---
+++ client/src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+const config = {
+    apiKey: "AIzaSyBRBx9rXAsyEkE59zOgSADT-wMFazakBag",
+    authDomain: "event-notification-syste-d8c65.firebaseapp.com",
+    databaseURL: "https://event-notification-syste-d8c65.firebaseio.com",
+    projectId: "event-notification-syste-d8c65",
+    storageBucket: "event-notification-syste-d8c65.appspot.com",
+    messagingSenderId: "274433923057",
+    appId: "1:274433923057:web:52ec259a8c63a6b699d650",
+    measurementId: "G-SEM2VBCXCD"
+}
+ firebase.initializeApp(config)
+
+ export default firebase
