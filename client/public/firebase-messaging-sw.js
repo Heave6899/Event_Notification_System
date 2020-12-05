e3a3441989996f38c4d5fe270718da96fbeeb1ab
Tanuj Gupta <vanshfpatel@outlook.com>
2020-12-05 19:54:44 +0530

    deleted extra files

--- client/public/firebase-messaging-sw.js
+++
@@ -1,8 +0,0 @@
-importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-messaging.js');
-
-firebase.initializeApp({
-    messagingSenderId: "274433923057",
-})
-
-const initMessaging = firebase.messaging()
